ce2ea87e3b9005fbcdefd7a522faa93e7a938fd6
Klarissa Andrade <klarissaamorais@gmail.com>
2023-06-09 02:03:03 -0300

    Add comandos de manipulação de arquivos

--- Program.cs
+++ Program.cs
@@ -1,45 +1,79 @@
 ﻿using System;
-namespace Atividade{
-    class Program{
-        static void Main(string[] args){
+namespace Atividade
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+
             float val_pag;
-            Console.WriteLine("Informar Nome");
-            string var_nome = Console.ReadLine();
-            Console.WriteLine("Informar Endereço");
-            string var_endereco = Console.ReadLine();
             Console.WriteLine("Informar Pessoa fisica(f) ou juridica(j)");
             string var_tipo = Console.ReadLine();
-            if(var_tipo == "f"){
-                // se for pessoa fisica
-                Pessoa_fisica pf = new Pessoa_fisica(); 
-                //pf tem o tipo Pessoa_fisica e vai levar toda a estrutura da classe Pessoa_fisica, é um objeto. Além de herdar os atributos da classe pai, Clientes.
-                pf.nome= var_nome;
-                pf.endereco= var_endereco;
+            if (var_tipo == "f")
+            {
+                Pessoa_fisica pf = new Pessoa_fisica();
                 Console.WriteLine("Informar CPF:");
-                pf.cpf= Console.ReadLine();
-                Console.WriteLine("Informar RG:");
-                pf.rg= Console.ReadLine();
+                pf.cpf = Console.ReadLine();
+                string caminhoArquivo = @"C:\Users\klari\OneDrive\Área de Trabalho\SENAI\Atividade\Dados_pf.txt";
+                bool cpfEncontrado = false;
+
+                using (StreamReader arquivo = new StreamReader(caminhoArquivo))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        if (linha == pf.cpf)
+                        {
+                            cpfEncontrado = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!cpfEncontrado)
+                {
+                    Console.WriteLine("CPF não encontrado no arquivo.");
+
+                    Console.WriteLine("Informar Nome:");
+                    pf.nome = Console.ReadLine();
+                    Console.WriteLine("Informar RG:");
+                    pf.rg = Console.ReadLine();
+                    Console.WriteLine("Informar Endereço:");
+                    pf.endereco = Console.ReadLine();
+
+                    using (StreamWriter arquivoEscrita = new StreamWriter(caminhoArquivo, true))
+                    {
+                        arquivoEscrita.WriteLine(pf.cpf);
+                        arquivoEscrita.WriteLine(pf.nome);
+                        arquivoEscrita.WriteLine(pf.rg);
+                        arquivoEscrita.WriteLine(pf.endereco);
+                    }
+                }
+                
                 Console.WriteLine("Informar valor de compra:");
-                val_pag= float.Parse(Console.ReadLine());
+                val_pag = float.Parse(Console.ReadLine());
                 pf.Pagar_Imposto(val_pag);
                 Console.WriteLine("----Pessoa Fisica-----");
                 Console.WriteLine("Total a pagar: " + pf.total.ToString("C"));
+
             }
-            if(var_tipo == "j"){
+
+
+            if (var_tipo == "j")
+            {
                 Pessoa_juridica pj = new Pessoa_juridica();
-                pj.nome= var_nome;
-                pj.endereco= var_endereco;
-                Console.WriteLine("Informar CPF:");
-                pj.cnpj= Console.ReadLine();
+                Console.WriteLine("Informar CNPJ:");
+                pj.cnpj = Console.ReadLine();
                 Console.WriteLine("Informar IE:");
-                pj.ie= Console.ReadLine(); 
+                pj.ie = Console.ReadLine();
+                Console.WriteLine("Informar Endereço");
+                pj.endereco = Console.ReadLine();
                 Console.WriteLine("Informar valor de compra:");
-                val_pag= float.Parse(Console.ReadLine());
+                val_pag = float.Parse(Console.ReadLine());
                 pj.Pagar_Imposto(val_pag);
                 Console.WriteLine("----Pessoa Juridica-----");
                 Console.WriteLine("Total a pagar: " + pj.total.ToString("C"));
             }
-
         }
     }
 }
